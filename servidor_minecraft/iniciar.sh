5c45fffcf53647eac361cdc4b8ccb3f3ef3b7d78
Codespace Backup <codespace@example.com>
2026-04-13 21:17:39 +0000

    Backup manual: 2026-04-13 21:17:39

--- servidor_minecraft/iniciar.sh
+++ servidor_minecraft/iniciar.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 
-# Asegúrate de cambiar el nombre del archivo .jar por el tuyo exacto.
-JAR_NAME="forge-1.12.2-14.23.5.2860.jar"
-
-java -Xms4G -Xmx4G -XX:+UseG1GC -XX:+UnlockExperimentalVMOptions -XX:MaxGCPauseMillis=50 -XX:+DisableExplicitGC -XX:TargetSurvivorRatio=90 -XX:G1NewSizePercent=50 -XX:G1MaxNewSizePercent=80 -XX:G1MixedGCLiveThresholdPercent=35 -XX:+AlwaysPreTouch -XX:+ParallelRefProcEnabled -jar $JAR_NAME nogui
+/usr/lib/jvm/java-8-openjdk-amd64/jre/bin/java -Xmx6G -Xms6G -jar forge-1.12.2-14.23.5.2860.jar nogui
