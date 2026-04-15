c26b7551778c0dd35dcfaaa32e8590e858de7b4f
MC Server Manager <mc-manager@example.com>
2026-04-15 22:25:47 +0000

    Backup manual: 2026-04-15 22:25:46

--- servidor_minecraft/iniciar.sh
+++ servidor_minecraft/iniciar.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-/usr/lib/jvm/java-8-openjdk-amd64/jre/bin/java -Xmx6G -Xms6G -jar forge-1.12.2-14.23.5.2860.jar nogui
+# Asignamos 10GB de RAM (dejando 6GB para el sistema)
+/usr/lib/jvm/java-8-openjdk-amd64/jre/bin/java -Xmx10G -Xms4G -XX:+UseG1GC -XX:+UnlockExperimentalVMOptions -XX:MaxGCPauseMillis=100 -XX:+DisableExplicitGC -XX:TargetSurvivorRatio=90 -XX:G1NewSizePercent=35 -XX:G1MaxNewSizePercent=60 -XX:G1MixedGCLiveThresholdPercent=90 -XX:+AlwaysPreTouch -jar forge-1.12.2-14.23.5.2860.jar nogui
